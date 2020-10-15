17ef2e9db23e335e88750e9ceef0cc5578b95a50
nabilazora <nabilazora@gmail.com>
2020-10-15 20:23:25 +0700

    quoteApi done

--- server/app.js
+++ server/app.js
@@ -4,10 +4,13 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3000
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const quoteApi = require('./quoteApi')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.get('/quoteApi', quoteApi)
 
 let users = []
 io.on('connection', (socket) => {
